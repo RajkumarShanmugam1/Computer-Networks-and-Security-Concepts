c7653e8087f7b7c234b965c58f0b269310a19eb3
Rajkumar S <76644058+Sriraj151@users.noreply.github.com>
2023-07-17 18:21:36 +0530

    Update client.c

--- 11 . Echo_server/Client.c
+++ 11 . Echo_server/Client.c
@@ -1,52 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<string.h>
-#include<unistd.h>
+/* TCP Socket - ECHO SERVER
+ * Client program
+ */
 
-int main()
-{
-   struct sockaddr_in ccin;
-   int i=0;
-   char buf[30];
-   int ccid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-   if(ccid == -1)
-       printf("cresation failed \n");
-   else
-   {
-      ccin.sin_family=AF_INET;
-      ccin.sin_port=htons(30163);
-      ccin.sin_addr.s_addr=inet_addr("127.0.0.1");
-      printf("creation successful\n");
-      int cnct=connect(ccid,(struct sockaddr * ) &ccin, sizeof(ccin));
-      if(cnct == -1)
-         printf("Connection failed\n");
-      else
-      {
-         printf("Connection successful\n");
-         char sent_msg[50];
-         buf[0] = 'a';
-         while(1)
-         {
-             printf("Mesg : ");
-             scanf("%s",&sent_msg);
-             int snt = send(ccid,sent_msg,30,0);
-             if(strcmp(sent_msg,"bye")==0)
-                   break;
-             if(snt == -1)
-                 printf("msg transmitted failed\n");
-             int resv=recv(ccid,buf,30,0);
-             if(resv == -1)
-                 printf("msg didnt received\n");
-             else
-                 printf("Echo : %s\n",buf);
-             for(i=0;i<strlen(buf);i++)
-                 buf[i]=' ';
-         }
-       }
-       int status = close(ccid);
-       if(status == 0)
-          printf("closed\n");
-   }
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define PORT      1234       	/* Your port */
+#define IP        "127.0.0.1"	/* Your IP address */
+#define BUFF_SIZE 100		   /* Size for buffer*/
+
+int main() {
+  int client_socket, connect_status, status;
+  char buffer[BUFF_SIZE], message[BUFF_SIZE];
+  struct sockaddr_in server_addr;
+
+  /* SOCKET */
+  client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (client_socket == -1) {
+    perror("SOCKET CREATION ERROR");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("SOCKET CREATED\n");
+
+  /* SERVER ADDRESS INITIALIZATION */
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = inet_addr(IP);
+
+  /* CONNECT */
+  connect_status = connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (connect_status == -1) {
+    perror("CONNECTION FAILED");
+    close(client_socket);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("CONNECTION SUCCESSFUL\n");
+
+  /* ECHO APPLICAION */
+  memset(&buffer,0,sizeof(buffer));
+  while(1) {
+    printf("\nMESSAGE : ");
+    scanf("%s",message);
+    if(send(client_socket, message, strlen(message), 0) == -1) {
+       perror("SEND ERROR");
+       break;
+    }
+    if(strcmp(message,"bye")==0) {
+       printf("\nSERVER DISCONNECTED\n");
+       break;
+    }
+    status = recv(client_socket, buffer, sizeof(buffer)-1 ,0);
+    if(status == -1) {
+       perror("RECEIVE ERROR");
+       break;
+    } 
+    else if(status == 0) {
+       printf("SERVER CLOSED THE CONNECTION\n");
+       break;
+    } 
+    else{
+       printf("\nREPLY   : %s\n",buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+  }
+
+  /* Close socket */
+  if(close(client_socket) == 0) {
+    printf("SOCKET CLOSED\n");
+  }
+  else {
+    perror("SOCKET CLOSE ERROR");
+  }
+
+  return 0;
 }
