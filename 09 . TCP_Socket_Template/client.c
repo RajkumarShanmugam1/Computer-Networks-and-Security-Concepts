17f68411a37f0d34e3ce14f1c783605dc180d31a
Rajkumar S <76644058+Sriraj151@users.noreply.github.com>
2023-07-17 17:23:21 +0530

    Update client.c

--- 09 . TCP_Socket_Template/client.c
+++ 09 . TCP_Socket_Template/client.c
@@ -1,31 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+/* TCP Socket - Template
+ * Client program
+ */
 
-int main()
-{
-   struct sockaddr_in ccin;
-   int ccid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-   if(ccid == -1)
-       printf("cresation failed \n");
-   else
-   {
-      ccin.sin_family=AF_INET;
-      ccin.sin_port=htons(30163);
-      ccin.sin_addr.s_addr=inet_addr("127.0.0.1");  // Your IP Address
-      printf("creation successful\n");
-      int cnct=connect(ccid,(struct sockaddr * ) &ccin, sizeof(ccin));
-      if(cnct == -1)
-         printf("Connection failed\n");
-      else
-      {
-         printf("Connection successful\n");
-         //Your Application
-      }
-      int status = close(ccid);
-      if(status == 0)
-          printf("closed\n");
-   }
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define PORT 1234       /* Your port */
+#define IP "127.0.0.1"  /* Your IP address */
+
+int main() {
+  int client_socket, connect_status;
+  struct sockaddr_in server_addr;
+
+  /* SOCKET */
+  client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (client_socket == -1) {
+    perror("SOCKET CREATION ERROR");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("SOCKET CREATED\n");
+
+  /* SERVER ADDRESS INITIALIZATION */
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = inet_addr(IP);
+
+  /* CONNECT */
+  connect_status = connect(client_socket, (struct sockaddr*) &server_addr, sizeof(server_addr));
+  if (connect_status == -1) {
+    perror("CONNECTION FAILED");
+    close(client_socket);
+    exit(EXIT_FAILURE);
+  }
+  printf("CONNECTION SUCCESSFUL\n");
+
+  /* Your application code */
+
+  /* SOCKET CLOSE */
+  if (close(client_socket) == 0) {
+    printf("SOCKET CLOSED\n");
+  }else {
+    perror("SOCKET CLOSE ERROR");
+  }
+
+  return 0;
 }
